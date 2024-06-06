7532566315123385fb3db54463f8466603cf89e7
danyal <danyallsty04@gmail.com>
2024-06-06 13:51:58 +0330

    input value of upload modal created

---
+++ components/Input.tsx
@@ -0,0 +1,42 @@
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type,placeholder, disabled, ...props }, ref) => {
+    return (
+      <input
+        title='input'
+        className={twMerge(
+          `
+        flex 
+        w-full
+        rounded-md
+        bg-neutral-700
+        border
+        border-transparent
+        p-3
+        text-sm
+        file:border-0
+        file:bg-transparent
+        file:text-sm
+        file:font-medium
+        placeholder:text-neutral-400
+        disabled:cursor-not-allowed
+        disabled:opacity-50
+        focus:outline-none
+        `,
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        type={type}
+        {...props}
+        placeholder={placeholder}
+      />
+    );
+  }
+);
+Input.displayName = "Input";
+export default Input;
