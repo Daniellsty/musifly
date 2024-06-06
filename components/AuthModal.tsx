10300ceb5579e4757bff09cb4473af421604cd50
danyal <danyallsty04@gmail.com>
2024-06-06 13:50:56 +0330

    fixed a bug for authModal

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,67 +1,62 @@
-'use client';
+"use client";
 
-import { useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-react";
-import {Auth} from '@supabase/auth-ui-react'
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
+import { Auth } from "@supabase/auth-ui-react";
 import { useRouter } from "next/navigation";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 import Modal from "./Modal";
 import useAuthModal from "@/hooks/UseAuthModal";
-import { createClient } from "@supabase/supabase-js";
 import { useEffect } from "react";
 
 const AuthModal = () => {
+  const supabaseClient = useSupabaseClient();
 
-    const supabaseUrl =process.env.NEXT_PUBLIC_SUPABASE_URL as string
-    const supabaseKey =process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  const router = useRouter();
+  const { session } = useSessionContext();
+  const { onClose, isOpen } = useAuthModal();
 
-    const supabaseClient = createClient(supabaseUrl,supabaseKey)
-
-    const router = useRouter();
-    const {session} = useSessionContext();
-    const {isOpen,onClose} =  useAuthModal();
-
-    const onChange =(open:boolean)=>{
-
-        if(!open){
-            onClose();
-        }
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
     }
+  };
 
-    useEffect(() => {
-        
-       if(session){
-        router.refresh();
-        onClose();
-       }
-    }, [session,router,onClose]);
+  useEffect(() => {
+    if (session) {
+      router.refresh();
+      onClose();
+    }
+  }, [session, router, onClose]);
 
   return (
     <Modal
-    title="welcome back"
-    description="Login to your account"
-    isOpen={isOpen}
-    onChange={onChange}
-    >
-        <Auth
+      title="welcome back"
+      description="Login to your account"
+      isOpen={isOpen}
+      onChange={onChange}>
+      <Auth
         theme="dark"
         magicLink
-        providers={['github']}
+        providers={["github"]}
         supabaseClient={supabaseClient}
         appearance={{
-            theme:ThemeSupa,
-            variables:{
-                default:{
-                    colors:{
-                        brand:"#404040",
-                        brandAccent:"#22c55e"
-                    }
-                }
-            }
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#404040",
+                brandAccent: "#22c55e",
+              },
+            },
+          },
         }}
-        />
+      />
     </Modal>
-  )
-}
+  );
+};
 
-export default AuthModal
+export default AuthModal;
