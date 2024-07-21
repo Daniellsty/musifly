d13d9073ad1faa9f916adc6cf9ecc464718616c1
danyal lsty <danyallsty04@gmail.com>
2024-07-21 14:08:17 +0330

    page conent of home page

---
+++ app/(site)/components/PageContent.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Song } from "@/types";
+import SongItem from "./SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
+
+interface PageContentProps {
+    songs:Song[];
+}
+
+const PageContent :React.FC<PageContentProps> = ({songs}) => {
+
+  const onPlay = useOnPlay(songs);
+
+  
+
+  return (
+    <div>
+          <div className="grid grid-cols-2
+          sm:grid-cols-3
+          md:grid-cols-4
+          lg:grid-cols-5
+          xl:grid-cols-8
+          gap-4
+          mt-4
+          ">
+              {
+                songs.map((song)=>{
+              
+                   
+                    return (
+                      
+                      <SongItem
+                      key={song.id}
+                      onClick={(id:string)=> onPlay(id)}
+                      data={song}
+                      />
+                    )
+                })
+              }
+            </div>
+    </div>
+  )
+}
+
+export default PageContent
