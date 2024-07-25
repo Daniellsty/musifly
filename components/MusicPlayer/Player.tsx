5199392b255e6ae9f591fb191f40ffd3dfce2fda
danyal lsty <danyallsty04@gmail.com>
2024-07-25 17:48:28 +0330

    main play component changed on ui

---
+++ components/MusicPlayer/Player.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
+import usePlayer from "@/hooks/usePlayer";
+import Index from "./Index";
+
+const Player = () => {
+  const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
+
+  const songUrl = useLoadSongUrl(song!);
+
+  if (!song) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div
+        className="
+    fixed
+    bottom-0 
+    w-full
+    py-3
+    h-[110px]
+    px-4
+    bg-white/5
+    bg-opacity-80 
+    backdrop-blur-sm 
+    
+    rounded-lg 
+    cursor-pointer
+    "
+      >
+        <Index key={songUrl} song={song} songUrl={songUrl} />
+      </div>
+      <div
+        className="
+    lg:hidden
+    block
+    "
+      >
+        <h1>content</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
